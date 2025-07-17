15e8d8b2b0cc388b7591ac1ecc6925246b880f4a
Tri Imam Wicaksono <triimamwicaksono@student.utwente.nl>
2025-07-17 14:47:21 +0200

    put mlflow into requirements

--- config/paths_config.py
+++ config/paths_config.py
@@ -5,3 +5,4 @@ RAW_DIR = "artifacts/raw"
 PROCESSED_DIR = "artifacts/processed"
 TRAIN_PATH = os.path.join(RAW_DIR,'titanic_train.csv')
 TEST_PATH = os.path.join(RAW_DIR,'titanic_test.csv')
+MODEL_OUTPUT_PATH = "artifacts/models/random_forest_model.pkl"
